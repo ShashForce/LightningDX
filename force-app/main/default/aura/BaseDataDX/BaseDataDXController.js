b424176b33162807ccf113f27373d12288b1192a
Shashank Srivatsavaya <shashank.srivatsavaya@gmail.com>
2017-08-14 17:40:08 +0530

    Added Quick Action

--- force-app/main/default/aura/BaseDataDX/BaseDataDXController.js
+++ force-app/main/default/aura/BaseDataDX/BaseDataDXController.js
@@ -1,5 +1,13 @@
 ({
     handleSave : function(component, event, helper) {
-        component.find("dsComp").saveRecord(function(result){});
+        component.find("dsComp").saveRecord(function(result){
+            //$A.get("e.force:navigateToSObject").setParams({"recordId":component.get("v.contact.Id")}).fire();
+            $A.get("e.force:closeQuickAction").fire();
+        });
+    },
+    handleRefresh : function(component, event, helper) {
+        if (event.getParams().changeType === "CHANGED") {
+            component.find("dsComp").reloadRecord();
+        }
     }
 })
