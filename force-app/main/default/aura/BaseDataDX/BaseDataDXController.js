109ccb0aceb7b0211e0458874f36adbe828b1009
Shashank Srivatsavaya <shashank.srivatsavaya@gmail.com>
2017-08-15 11:59:10 +0530

    Simplified

--- force-app/main/default/aura/BaseDataDX/BaseDataDXController.js
+++ force-app/main/default/aura/BaseDataDX/BaseDataDXController.js
@@ -1,13 +1,6 @@
 ({
     handleSave : function(component, event, helper) {
-        component.find("dsComp").saveRecord(function(result){
-            //$A.get("e.force:navigateToSObject").setParams({"recordId":component.get("v.contact.Id")}).fire();
-            $A.get("e.force:closeQuickAction").fire();
-        });
-    },
-    handleRefresh : function(component, event, helper) {
-        if (event.getParams().changeType === "CHANGED") {
-            component.find("dsComp").reloadRecord();
-        }
+        //invoke Data Service's inbuilt saveRecord function
+        component.find("DataService").saveRecord(function(result){});
     }
 })
